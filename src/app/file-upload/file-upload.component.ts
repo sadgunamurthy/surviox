643f6f298119f3c32682fb32df83f5dab637775c
sadgunamurthy <sadgunamurthy52@gmail.com>
2024-06-05 17:39:37 +0530

    file upload

--- src/app/file-upload/file-upload.component.ts
+++ src/app/file-upload/file-upload.component.ts
@@ -36,34 +36,7 @@ export class FileUploadComponent implements OnInit {
   deleteFile(index: number): void {
     this.files.splice(index, 1);
   }
-  // triggerFileInput(): void {
-  //   const fileInput = document.getElementById('fileInput') as HTMLInputElement;
-  //   fileInput.click();
-  // }
 
-  // onFileSelected(event: Event): void {
-  //   const input = event.target as HTMLInputElement;
-  //   if (input.files && input.files.length > 0) {
-  //     this.selectedFile = input.files[0];
-  //     this.onUpload();
-  //   }
-  // }
-
-  // onUpload(): void {
-  //   if (this.selectedFile) {
-  //     const formData = new FormData();
-  //     formData.append('file', this.selectedFile, this.selectedFile.name);
-
-  //     this.http.post('YOUR_BACKEND_URL_HERE', formData).subscribe(
-  //       (response) => {
-  //         console.log('Upload successful:', response);
-  //       },
-  //       (error) => {
-  //         console.error('Upload failed:', error);
-  //       }
-  //     );
-  //   }
-  // }
   ngOnInit(): void {
   }
 
